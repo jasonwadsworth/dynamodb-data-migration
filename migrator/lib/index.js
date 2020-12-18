8e636036924c84c0ca76abbcf66e1a77e3d69f5f
Jason Wadsworth <jasonwadsworth@outlook.com>
2020-12-17 20:57:53 -0600

    Removed lib

--- migrator/lib/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Migrator = void 0;
-var Migrator_1 = require("./Migrator");
-Object.defineProperty(exports, "Migrator", { enumerable: true, get: function () { return Migrator_1.Migrator; } });
-//# sourceMappingURL=index.js.map
